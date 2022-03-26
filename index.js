5c6ec16f631fe3b056bf5f817f11f814a52a9842
jerline01 <jerlish97@gmail.com>
2022-03-26 17:10:59 +0530

    fruits added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const MongoClient = require("mongodb").MongoClient;
 
-// database connection dgfsf
+// database connection 
 connection();
 
 app.use(express.json())
@@ -17,24 +17,28 @@ app.use("/api/users",userRoutes);
 app.use("/api/auth",authRoutes);
 
 const MONGO_URL = process.env.DB;
+const client = new MongoClient(MONGO_URL);
 async function createConnection() {
-    const client = new MongoClient(MONGO_URL);
+    
     await client.connect();
     console.log("Mongo is connected 😊");
     return client;
   }
-   const client = createConnection();
+   //const client = createConnection().then((res)=> res);
+createConnection().then(()=>{
+    app.get("/fruits", async function(request,response){
+        console.log("fruits running Started");
+        console.log(client);
+        const result = await client 
+          .db("b30wd")
+          .collection("fruits")
+          .find({})
+          .toArray();
+          console.log("fruits running successfully");
+        response.send(result);
+      });
+})
 
-app.get("/fruits", async function(request,response){
-    console.log("fruits running Started");
-    const result = await  new MongoClient(MONGO_URL).connect() 
-      .db("b30wd")
-      .collection("fruits")
-      .find({})
-      .toArray();
-      console.log("fruits running successfully");
-    response.send(result);
-  });
 
   const port = process.env.PORT || 8080;
 app.listen(port, ()=>console.log(`Server started in port ${port}`));
